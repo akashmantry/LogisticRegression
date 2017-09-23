5bf8bd37d86f2950c3d7cadca1f2cc487f42430e
akashmantry <akashmantry@gmail.com>
2017-09-23 17:03:49 -0400

    added spam detection

--- spam_detection.py
+++ spam_detection.py
@@ -11,14 +11,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from random import randrange
 
-"""
-   Benign: 458 (65.5%)
-   Malignant: 241 (34.5%)
-   16 missing data points
-   0 beningn
-   1 malignant
-"""
-dataset = pd.read_csv('breast_cancer.csv', sep='\t')
+
+dataset = pd.read_csv('spambase.csv')
 dataset.replace('?', np.nan, inplace=True)
 dataset.drop(['ID'], 1, inplace=True)
 dataset.dropna(inplace=True)
@@ -45,9 +39,10 @@ def sigmoid(X):
     return 1/(1 + np.exp(- X))
     
 def pre_processing(matrix):
-    range_ = 10
-    b = np.apply_along_axis(lambda x: (x-np.mean(x))/range_, 0, matrix)
+    matrix_copy = matrix     
+    b = np.apply_along_axis(lambda x: (x-np.mean(x))/float(np.std(x)),0,matrix_copy)
     return b
+    
 
 def cost_function(X, y, theta):
     h_theta = sigmoid(np.dot(X, theta))
@@ -64,10 +59,9 @@ def calculate_gradient(X, y, theta, index, X_count):
 
 def gradient_descent(training_set, alpha, max_iterations, plot_graph):
     iter_count = 0
-
     training_set = np.asarray(training_set)
-    X = training_set.T[0:9].T
-    y = training_set.T[9].T
+    X = training_set.T[0:57].T
+    y = training_set.T[57].T
     X_count = X.shape[1]
 
     theta = np.zeros(X_count)
@@ -83,28 +77,26 @@ def gradient_descent(training_set, alpha, max_iterations, plot_graph):
                 prediction += (regularization_parameter/X_count)*prev_theta
             theta[i] = prev_theta - alpha * prediction
             
-            mean = cost_function(X, y, theta)
-            
             if plot_graph:
+                mean = cost_function(X, y, theta)
                 x_vals.append(iter_count)
                 y_vals.append(mean)
     
     if plot_graph:
         plt.suptitle("Gradient Descent plot")
-        plt.gca().invert_yaxis()
         plt.plot(x_vals,y_vals)
         plt.xlabel("Iteration ")
         plt.ylabel("Cost function J(theta)")
-        #plt.show()
-        fileName = "gradient_descent_plot.png";
-        plt.savefig(fileName, bbox_inches='tight')
+        plt.show()
+        #fileName = "gradient_descent_plot.png";
+        #plt.savefig(fileName, bbox_inches='tight')
     
     return theta
             
 def compute_efficiency(test_set, theta):
     test_set = np.asarray(test_set)
-    X = test_set.T[0:9].T
-    y = test_set.T[9].T
+    X = test_set.T[0:57].T
+    y = test_set.T[57].T
     X_count = X.shape[0]
     correct = 0
     
@@ -142,5 +134,5 @@ X = pre_processing(X)
 reshaped_y = y.reshape(y.shape[0], -1)
 processed_dataset = np.concatenate((X, reshaped_y), axis=1)
 results = evaluate_algorithm(processed_dataset.tolist(), n_folds=10,
-                             alpha=0.01, max_iterations=500, plot_graph=False)
+                             alpha=.00004, max_iterations=100, plot_graph=True)
 print("Mean : ",np.mean(results))
